20ad35af99615711c89938fd17798824b35eed1d
Your Name <your.email@example.com>
2025-06-11 23:05:21 -0400

    v2 clean up

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
-/// <reference types="vite/client" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  base: "/my-portfolio/",
+  plugins: [react()],
+});
